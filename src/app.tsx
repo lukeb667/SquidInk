67e387761ee5da7346141c0c27d179c93e42b132
lb667 <lb667@proton.me>
2025-03-31 15:21:23 -0400

    Added test basic editor

--- src/app.tsx
+++ src/app.tsx
@@ -1,44 +1,12 @@
-import { Button } from "@heroui/button"
-import { useState } from "react"
+import Tiptap from './tipTapTest'
 
-import reactLogo from "@/assets/react.svg"
-import viteLogo from "/electron-vite.animate.svg"
-
-export default function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<div className="flex justify-center items-center">
-				<a href="https://electron-vite.github.io" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-
-			<h1>Vite + React</h1>
-
-			<div className="card flex flex-col justify-center items-center gap-y-4">
-				<Button
-					className="dark w-1/2"
-					color="default"
-					radius="full"
-					variant="shadow"
-					onPress={ () => setCount((count) => count + 1) }
-				>
-					count is {count}
-				</Button>
-
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+const App = () => {
+  return (
+    <div className="card">
+      <Tiptap />
+	</div>
+	
+  )
 }
+
+export default App
