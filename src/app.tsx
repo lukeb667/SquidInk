55589283f44e013fb68cce564c3865c31bfa6e97
Ryan Smith <rysmith2113@gmail.com>
2025-04-10 10:12:49 -0400

    Restructure the app layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
-import Tiptap from './tipTapTest'
+import { JSX } from "react";
 
-const App = () => {
-  return (
-    <div className="card">
-      <Tiptap />
-	</div>
-	
-  )
+import RichTextEditor from "@/components/editor";
+
+export default function App(): JSX.Element {
+	return (
+		<div className="flex flex-col min-h-screen w-screen">
+			<main>
+				<RichTextEditor/>
+			</main>
+		</div>
+	);
 }
-
-export default App
